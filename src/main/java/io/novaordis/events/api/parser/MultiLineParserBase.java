8885015eb7dafe5aab97df720817426181a8421a
Ovidiu Feodorov <ovidiu@novaordis.com>
2017-04-20 12:06:40 -0400

    moving generic G1Parser functionality into superclass

--- src/main/java/io/novaordis/events/api/parser/MultiLineParserBase.java
+++ src/main/java/io/novaordis/events/api/parser/MultiLineParserBase.java
@@ -16,29 +16,171 @@
 
 package io.novaordis.events.api.parser;
 
+import io.novaordis.events.api.event.Event;
+import io.novaordis.events.api.gc.GCEvent;
+import io.novaordis.events.api.gc.GCParsingException;
+import io.novaordis.events.gc.g1.G1EventFactory;
+import io.novaordis.events.gc.g1.GCEventStartMarker;
+import io.novaordis.events.gc.g1.RawGCEvent;
+import io.novaordis.events.gc.g1.Time;
+import io.novaordis.utilities.time.Timestamp;
+import io.novaordis.utilities.time.TimestampImpl;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author Ovidiu Feodorov <ovidiu@novaordis.com>
  * @since 2/14/17
  */
 public abstract class MultiLineParserBase implements MultiLineGCParser {
 
-
     // Constants -------------------------------------------------------------------------------------------------------
 
+    public static final String PrintGCDateStamps_TIMESTAMP_STRING_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
+
+    public static final DateFormat PrintGCDateStamps_TIMESTAMP_FORMAT =
+            new SimpleDateFormat(PrintGCDateStamps_TIMESTAMP_STRING_FORMAT);
+
+    public static final Pattern PrintGCDateStamps_PATTERN = Pattern.compile(
+            "[1-2][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]T[0-9][0-9]:[0-9][0-9]:[0-9][0-9]\\.[0-9][0-9][0-9][-|+][0-9][0-9][0-9][0-9]");
+
+    //
+    // relative time since the GC collection started, in seconds followed by milliseconds ("7.896:")
+    //
+    public static final Pattern PrintGCTimeStamps_PATTERN = Pattern.compile("([0-9]+\\.[0-9][0-9][0-9]):");
+
     // Static ----------------------------------------------------------------------------------------------------------
+
+    /**
+     * Identifies a GC event start marker, from the position indicated by the current marker.
+     *
+     * @param current the current marker. May be null, in which case we start looking from the top of the line.
+     *
+     * @return the start marker or null if no marker has been identified.
+     */
+    public static GCEventStartMarker identifyGCEventStartMarker(String line, GCEventStartMarker current)
+            throws GCParsingException {
+
+        int from = current == null ? 0 : current.getContentStart();
+
+        String interestingSection = line.substring(from);
+        Matcher m = PrintGCDateStamps_PATTERN.matcher(interestingSection);
+
+        if (!m.find()) {
+
+            //
+            // pattern not found in the interesting section
+            //
+
+            return null;
+        }
+
+        String dateStamp = m.group();
+
+        Timestamp timestamp;
+
+        //
+        // parse it immediately, we know we matched the DateStamps pattern
+        //
+
+        try {
+
+            timestamp = new TimestampImpl(dateStamp, PrintGCDateStamps_TIMESTAMP_FORMAT);
+        }
+        catch (ParseException e) {
+
+            throw new GCParsingException("failed to parse date stamp \"" + dateStamp + "\" into a date", e);
+        }
+
+        String rest = interestingSection.substring(m.start() + dateStamp.length());
+
+        int contentStart = from + m.end();
+
+        //
+        // look for offset information
+        //
+
+        Matcher m2 = PrintGCTimeStamps_PATTERN.matcher(rest);
+
+        Long offset = null;
+
+        if (m2.find()) {
+
+            String os = m2.group(1);
+            offset = 1000L * Integer.parseInt(os.substring(0, os.length() - 4));
+            offset += Integer.parseInt(os.substring(os.length() - 3));
+            contentStart += m2.end();
+        }
+
+        Time t = new Time(timestamp, offset);
+
+
+        //
+        // discard empty spaces before the content
+        //
+        while(line.charAt(contentStart) == ' ') {
+
+            contentStart++;
+        }
+
+        return new GCEventStartMarker(t, from + m.start(), contentStart);
+    }
+
+    public static List<Event> toEventList(List<RawGCEvent> rawGCEvents) throws GCParsingException {
+
+        if (rawGCEvents.isEmpty()) {
+
+            return Collections.emptyList();
+        }
+
+        List<Event> result = new ArrayList<>();
+
+        for(RawGCEvent re: rawGCEvents) {
+
+            GCEvent e = G1EventFactory.build(re);
+            result.add(e);
+        }
+
+        return result;
+    }
 
     // Attributes ------------------------------------------------------------------------------------------------------
 
     private long lineNumber;
+    private List<RawGCEvent> completedEvents;
+    private RawGCEvent currentEvent;
 
     // Constructors ----------------------------------------------------------------------------------------------------
 
     protected MultiLineParserBase() {
 
         this.lineNumber = 0;
+        this.completedEvents = new ArrayList<>();
+        this.currentEvent = null;
     }
 
     // MultiLineParser implementation ----------------------------------------------------------------------------------
+
+    @Override
+    public List<Event> parse(String line) throws GCParsingException {
+
+        List<RawGCEvent> rawGCEvents = processLine(line);
+        return toEventList(rawGCEvents);
+    }
+
+    @Override
+    public List<Event> close() throws GCParsingException {
+
+        List<RawGCEvent> rawGCEvents = closeInternal();
+        return toEventList(rawGCEvents);
+    }
 
     // Public ----------------------------------------------------------------------------------------------------------
 
@@ -60,14 +202,111 @@ public abstract class MultiLineParserBase implements MultiLineGCParser {
 
     // Package protected -----------------------------------------------------------------------------------------------
 
-    // Protected -------------------------------------------------------------------------------------------------------
+    List<RawGCEvent> closeInternal() {
 
-    protected void incrementLineNumber() {
+        if (currentEvent != null) {
+
+            completedEvents.add(currentEvent);
+            currentEvent = null;
+        }
+
+        List<RawGCEvent> result = completedEvents;
+        completedEvents = new ArrayList<>();
+        return result;
+    }
+
+    List<RawGCEvent> processLine(String line) throws GCParsingException {
+
+        incrementLineNumber();
+
+        GCEventStartMarker currentMarker = null;
+
+        //
+        // be prepared to handle the situation when multiple events are on the same line
+        //
+
+        while(true) {
+
+            GCEventStartMarker newMarker = identifyGCEventStartMarker(line, currentMarker);
+
+            updateCurrentEvent(line, currentMarker, newMarker);
+
+            if (newMarker != null) {
+
+                if (currentEvent != null) {
+
+                    completedEvents.add(currentEvent);
+                }
+                currentMarker = newMarker;
+                currentEvent = new RawGCEvent(currentMarker.getTime(), getLineNumber());
+
+            }
+            else {
+
+                //
+                // done with this line
+                //
+                break;
+            }
+        }
+
+        if (completedEvents.isEmpty()) {
+
+            return Collections.emptyList();
+        }
+
+        List<RawGCEvent> result = completedEvents;
+        completedEvents = new ArrayList<>();
+        return result;
+    }
+
+    void incrementLineNumber() {
 
         lineNumber ++;
     }
 
+    /**
+     * For testing only.
+     */
+    RawGCEvent getCurrentEvent() {
+
+        return currentEvent;
+    }
+
+    /**
+     * For testing only.
+     */
+    List<RawGCEvent> getCompletedEvents() {
+
+        return completedEvents;
+    }
+
+    // Protected -------------------------------------------------------------------------------------------------------
+
     // Private ---------------------------------------------------------------------------------------------------------
+
+    /**
+     * Updates the content of the current event (if any) with the content associated with the current marker, if any.
+     */
+    private void updateCurrentEvent(String line, GCEventStartMarker currentMarker, GCEventStartMarker newMarker) {
+
+        if (currentEvent == null) {
+
+            return;
+        }
+
+        //
+        // append the content between the current marker and the new marker
+        //
+
+        int from = currentMarker == null ? 0 : currentMarker.getContentStart();
+        int to = newMarker == null ? line.length() : newMarker.getEventStart();
+        String content = line.substring(from, to);
+        currentEvent.append(content);
+        if (newMarker == null) {
+            currentEvent.append("\n");
+        }
+    }
 
     // Inner classes ---------------------------------------------------------------------------------------------------
 
--- src/main/java/io/novaordis/events/gc/g1/G1Parser.java
+++ src/main/java/io/novaordis/events/gc/g1/G1Parser.java
@@ -16,22 +16,8 @@
 
 package io.novaordis.events.gc.g1;
 
-import io.novaordis.events.api.event.Event;
-import io.novaordis.events.api.gc.GCEvent;
-import io.novaordis.events.api.gc.GCParsingException;
 import io.novaordis.events.api.parser.MultiLineParserBase;
 import io.novaordis.events.gc.CollectorType;
-import io.novaordis.utilities.time.Timestamp;
-import io.novaordis.utilities.time.TimestampImpl;
-
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  *
@@ -44,128 +30,15 @@ public class G1Parser extends MultiLineParserBase {
 
     // Constants -------------------------------------------------------------------------------------------------------
 
-    public static final String PrintGCDateStamps_TIMESTAMP_STRING_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
-
-    public static final DateFormat PrintGCDateStamps_TIMESTAMP_FORMAT =
-            new SimpleDateFormat(PrintGCDateStamps_TIMESTAMP_STRING_FORMAT);
-
-    public static final Pattern PrintGCDateStamps_PATTERN = Pattern.compile(
-            "[1-2][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]T[0-9][0-9]:[0-9][0-9]:[0-9][0-9]\\.[0-9][0-9][0-9][-|+][0-9][0-9][0-9][0-9]");
-
-    //
-    // relative time since the GC collection started, in seconds followed by milliseconds ("7.896:")
-    //
-    public static final Pattern PrintGCTimeStamps_PATTERN = Pattern.compile("([0-9]+\\.[0-9][0-9][0-9]):");
-
-
     // Static ----------------------------------------------------------------------------------------------------------
 
-    /**
-     * Identifies a GC event start marker, from the position indicated by the current marker.
-     *
-     * @param current the current marker. May be null, in which case we start looking from the top of the line.
-     *
-     * @return the start marker or null if no marker has been identified.
-     */
-    public static GCEventStartMarker identifyGCEventStartMarker(String line, GCEventStartMarker current)
-            throws GCParsingException {
-
-        int from = current == null ? 0 : current.getContentStart();
-
-        String interestingSection = line.substring(from);
-        Matcher m = PrintGCDateStamps_PATTERN.matcher(interestingSection);
-
-        if (!m.find()) {
-
-            //
-            // pattern not found in the interesting section
-            //
-
-            return null;
-        }
-
-        String dateStamp = m.group();
-
-        Timestamp timestamp;
-
-        //
-        // parse it immediately, we know we matched the DateStamps pattern
-        //
-
-        try {
-
-            timestamp = new TimestampImpl(dateStamp, PrintGCDateStamps_TIMESTAMP_FORMAT);
-        }
-        catch (ParseException e) {
-
-            throw new GCParsingException("failed to parse date stamp \"" + dateStamp + "\" into a date", e);
-        }
-
-        String rest = interestingSection.substring(m.start() + dateStamp.length());
-
-        int contentStart = from + m.end();
-
-        //
-        // look for offset information
-        //
-
-        Matcher m2 = PrintGCTimeStamps_PATTERN.matcher(rest);
-
-        Long offset = null;
-
-        if (m2.find()) {
-
-            String os = m2.group(1);
-            offset = 1000L * Integer.parseInt(os.substring(0, os.length() - 4));
-            offset += Integer.parseInt(os.substring(os.length() - 3));
-            contentStart += m2.end();
-        }
-
-        Time t = new Time(timestamp, offset);
-
-
-        //
-        // discard empty spaces before the content
-        //
-        while(line.charAt(contentStart) == ' ') {
-
-            contentStart++;
-        }
-
-        return new GCEventStartMarker(t, from + m.start(), contentStart);
-    }
-
-    public static List<Event> toEventList(List<RawGCEvent> rawGCEvents) throws GCParsingException {
-
-        if (rawGCEvents.isEmpty()) {
-
-            return Collections.emptyList();
-        }
-
-        List<Event> result = new ArrayList<>();
-
-        for(RawGCEvent re: rawGCEvents) {
-
-            GCEvent e = G1EventFactory.build(re);
-            result.add(e);
-        }
-
-        return result;
-    }
-
     // Attributes ------------------------------------------------------------------------------------------------------
-
-    private List<RawGCEvent> completedEvents;
-    private RawGCEvent currentEvent;
 
     // Constructors ----------------------------------------------------------------------------------------------------
 
     public G1Parser() {
 
         super();
-
-        this.completedEvents = new ArrayList<>();
-        this.currentEvent = null;
     }
 
     // MultiLineParser implementation ----------------------------------------------------------------------------------
@@ -176,124 +49,13 @@ public class G1Parser extends MultiLineParserBase {
         return CollectorType.G1;
     }
 
-    @Override
-    public List<Event> parse(String line) throws GCParsingException {
-
-        List<RawGCEvent> rawGCEvents = processLine(line);
-        return toEventList(rawGCEvents);
-    }
-
-    @Override
-    public List<Event> close() throws GCParsingException {
-
-        List<RawGCEvent> rawGCEvents = closeInternal();
-        return toEventList(rawGCEvents);
-    }
-
     // Public ----------------------------------------------------------------------------------------------------------
 
     // Package protected -----------------------------------------------------------------------------------------------
 
     // Protected -------------------------------------------------------------------------------------------------------
 
-    List<RawGCEvent> processLine(String line) throws GCParsingException {
-
-        incrementLineNumber();
-
-        GCEventStartMarker currentMarker = null;
-
-        //
-        // be prepared to handle the situation when multiple events are on the same line
-        //
-
-        while(true) {
-
-            GCEventStartMarker newMarker = identifyGCEventStartMarker(line, currentMarker);
-
-            updateCurrentEvent(line, currentMarker, newMarker);
-
-            if (newMarker != null) {
-
-                if (currentEvent != null) {
-
-                    completedEvents.add(currentEvent);
-                }
-                currentMarker = newMarker;
-                currentEvent = new RawGCEvent(currentMarker.getTime(), getLineNumber());
-
-            }
-            else {
-
-                //
-                // done with this line
-                //
-                break;
-            }
-        }
-
-        if (completedEvents.isEmpty()) {
-
-            return Collections.emptyList();
-        }
-
-        List<RawGCEvent> result = completedEvents;
-        completedEvents = new ArrayList<>();
-        return result;
-    }
-
-    List<RawGCEvent> closeInternal() {
-
-        if (currentEvent != null) {
-
-            completedEvents.add(currentEvent);
-            currentEvent = null;
-        }
-
-        List<RawGCEvent> result = completedEvents;
-        completedEvents = new ArrayList<>();
-        return result;
-    }
-
-    /**
-     * For testing only.
-     */
-    RawGCEvent getCurrentEvent() {
-
-        return currentEvent;
-    }
-
-    /**
-     * For testing only.
-     */
-    List<RawGCEvent> getCompletedEvents() {
-
-        return completedEvents;
-    }
-
     // Private ---------------------------------------------------------------------------------------------------------
-
-    /**
-     * Updates the content of the current event (if any) with the content associated with the current marker, if any.
-     */
-    private void updateCurrentEvent(String line, GCEventStartMarker currentMarker, GCEventStartMarker newMarker) {
-
-        if (currentEvent == null) {
-
-            return;
-        }
-
-        //
-        // append the content between the current marker and the new marker
-        //
-
-        int from = currentMarker == null ? 0 : currentMarker.getContentStart();
-        int to = newMarker == null ? line.length() : newMarker.getEventStart();
-        String content = line.substring(from, to);
-        currentEvent.append(content);
-        if (newMarker == null) {
-            currentEvent.append("\n");
-        }
-    }
 
     // Inner classes ---------------------------------------------------------------------------------------------------
 
